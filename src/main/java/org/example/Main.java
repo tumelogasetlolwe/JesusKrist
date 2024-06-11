5e136243dff977121e7912f54e30322145e57bcf
tumelogasetlolwe <ttyeyo@gmail.com>
2024-06-11 10:26:50 +0200

    Gasetlolwe

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("Tumelo Geee");
+    }
+
+    public String Nhlanhla(){
+
+
+        return null;
+    }
+
+    public String Teee(){
+
+
+        return null;
+    }
+}
